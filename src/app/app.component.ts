9b5f0a56e3fed1f23739e14eeac9a1b0933d9568
jrferguson <1q2w3e4r5t6y>
2023-05-11 23:27:26 +0200

    setup working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     title = 'dansah-web';
+
+    ngAfterViewInit(): void {
+      // import('../assets/js/jquery-3.2.1.min.js');
+      // import('../assets/js/blog_custom.js');
+      // import('../assets/js/contact_custom.js');
+      // import('../assets/js/custom.js');
+      // import('../assets/js/elements_custom.js');
+      // import('../assets/js/ministries_custom.js');
+      // import('../assets/js/sermon_single_custom.js');
+      // import('../assets/js/sermons_custom.js');
+
+    }
 }
