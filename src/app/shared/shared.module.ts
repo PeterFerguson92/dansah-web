13e0f52b79fb1339b15e8f50cfedd70721cf842d
jrferguson <1q2w3e4r5t6y>
2023-05-12 11:47:52 +0200

    added page title in progress

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
 
 @NgModule({
-    declarations: [NavBarComponent, FooterComponent],
+    declarations: [NavBarComponent, FooterComponent, PageTitleComponent],
     imports: [CommonModule],
-    exports: [NavBarComponent, FooterComponent],
+    exports: [NavBarComponent, FooterComponent, PageTitleComponent],
 })
 export class SharedModule {}
