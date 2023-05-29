c739460f9cf4002e0eb2eeace5d824a6c1796743
jrferguson <1q2w3e4r5t6y>
2023-05-29 04:59:30 +0100

    added media section

--- src/app/sections/media-section/media-section.component.ts
+++ src/app/sections/media-section/media-section.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-media-section',
-  templateUrl: './media-section.component.html',
-  styleUrls: ['./media-section.component.scss']
+    selector: 'app-media-section',
+    templateUrl: './media-section.component.html',
+    styleUrls: ['./media-section.component.scss'],
 })
 export class MediaSectionComponent implements OnInit {
+    title = 'Media title';
+    subtitle = 'Media Subtitle';
+    paragraphTitle = 'Lorem Ipsum Is Simply Dummy Text Of The Printing Industry.';
+    description1 =
+        // tslint:disable-next-line:max-line-length
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.';
+    description2 =
+        // tslint:disable-next-line:max-line-length
+        'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.';
 
-  constructor() { }
+    socialMediaData = [
+        {
+            iconName: 'fab fa-facebook',
+            title: 'Follow us on facebook',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            redirectLink: 'https://www.facebook.com/DanielKAnsahofficial',
+        },
+        {
+            iconName: 'fab fa-youtube',
+            title: 'Visit our youtube channel',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            redirectLink: 'https://www.youtube.com/@totalvictorytv8863',
+        },
+        {
+            iconName: 'fab fa-audible',
+            title: 'Listen to our podcast',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            redirectLink: 'https://www.apple.com/uk/apple-podcasts/',
+        },
+        {
+            iconName: 'fab fa-instagram',
+            title: 'Connect on instagram',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            redirectLink: 'https://www.instagram.com/danielkansah/',
+        },
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {}
 
+    ngOnInit() {}
+
+    getClass(iconName) {
+        return iconName + ' fa-3x';
+    }
+
+    redirect(link) {
+        window.open(link, '_blank');
+    }
 }
