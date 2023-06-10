15841686b02e38462d73f1295a704b0434505fc6
jrferguson <1q2w3e4r5t6y>
2023-06-10 16:25:26 +0100

    added section management

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+    sections = [
+        {
+            name: 'quoteOfTheDay',
+            isVisible: true,
+        },
+    ];
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    isSectionVisible(componentName) {
+        const section = this.getSectionByName(componentName);
+        return section.isVisible;
+    }
 
+    showSection(value, componentName) {
+        for (const section of this.sections) {
+            if (section.name === componentName) {
+                section.isVisible = value;
+            }
+        }
+    }
+
+    getSectionByName(componentName) {
+        return this.sections.find((o) => o.name === componentName);
+    }
 }
--- src/app/sections/quote-of-the-day/quote-of-the-day.component.ts
+++ src/app/sections/quote-of-the-day/quote-of-the-day.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
@@ -7,25 +7,31 @@ import { DataService } from 'src/app/shared/service/data.service';
     styleUrls: ['./quote-of-the-day.component.scss'],
 })
 export class QuoteOfTheDayComponent implements OnInit {
-    title = 'Quote of the day';
-    subtitle = 'God love us all';
-    backgroundImage = '../../../assets/images/church_5.png';
-    iconImgPath = '../../../assets/images/church_5.png';
-    text = `4 One thing I ask from the LORD, this only do I seek: that I may dwell in the house of the LORD all the days of my life,
-        to gaze on the beauty of the LORD and to seek him in his temple.`;
-    source = 'Psalm 27:4';
-
+    title;
+    subtitle;
+    backgroundImage;
+    iconImgPath;
+    text;
+    source;
+    @Output()
+    isDataRetrieved = new EventEmitter<boolean>();
     constructor(private service: DataService) {}
 
     ngOnInit() {
-        this.service.getQuoteOfTheDay().subscribe((data) => {
-            console.log(data);
-            this.title = data.quote_of_the_day[0].title;
-            this.subtitle = data.quote_of_the_day[0].sub_title;
-            this.backgroundImage = data.quote_of_the_day[0].background_icon_image_path;
-            this.iconImgPath = data.quote_of_the_day[0].icon_image_path;
-            this.text = data.quote_of_the_day[0].text;
-            // this.source = data.quote_of_the_day[0].title; //TODO missing source
-        });
+        this.service.getQuoteOfTheDay().subscribe(
+            (data) => {
+                this.isDataRetrieved.emit(true);
+                this.title = data.quote_of_the_day[0].title;
+                this.subtitle = data.quote_of_the_day[0].sub_title;
+                this.backgroundImage = data.quote_of_the_day[0].background_icon_image_path;
+                this.iconImgPath = data.quote_of_the_day[0].icon_image_path;
+                this.text = data.quote_of_the_day[0].text;
+                this.source = data.quote_of_the_day[0].source;
+            },
+            (error) => {
+                console.log(error);
+                this.isDataRetrieved.emit(false);
+            }
+        );
     }
 }
