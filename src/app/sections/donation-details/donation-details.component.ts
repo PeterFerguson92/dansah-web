b361e98eabe710e0323352543cb00c1cec09ad68
jrferguson <1q2w3e4r5t6y>
2024-01-07 20:53:03 +0000

    testing in progress

--- src/app/sections/donation-details/donation-details.component.ts
+++ src/app/sections/donation-details/donation-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaystackOptions } from 'angular4-paystack';
 
 @Component({
@@ -10,19 +11,27 @@ export class DonationDetailsComponent implements OnInit {
     @Input() data;
     path = '../../../assets/images/wallpa.png';
     title = '';
-
+    donateForm: FormGroup;
     options: PaystackOptions = {
-        amount: 50000,
-        email: 'user@mail.com',
-        ref: `${Math.ceil(Math.random() * 10e10)}`,
+        amount: -1,
+        email: null,
+        ref: null,
         currency: 'ZAR',
     };
-    constructor() {}
+    submittedData = null;
 
-    ngOnInit() {}
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit() {
+        this.donateForm = this.fb.group({
+            fullname: ['', [Validators.required, Validators.minLength(2)]],
+            email: ['', [Validators.required, Validators.email]],
+            amount: ['', Validators.required],
+        });
+    }
 
     onAdditionalLink() {
-        window.open('https://buy.stripe.com/14k6pW2r95mg2oEaEF', '_blank');
+        window.open('https://paystack.com/pay/lu3t5nwy6k', '_blank');
     }
 
     paymentInit() {
@@ -36,5 +45,14 @@ export class DonationDetailsComponent implements OnInit {
 
     paymentCancel() {
         console.log('payment failed');
+    }
+
+    get formControls() {
+        return this.donateForm.controls;
+    }
+
+    onSubmit() {
+        this.options = this.donateForm.value;
+        this.submittedData = JSON.stringify(this.donateForm.value, null, 2);
     }
 }
