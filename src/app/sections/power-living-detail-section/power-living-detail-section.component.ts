3c8207d15191a85f043365ebcbcabcc7893a2520
jrferguson <1q2w3e4r5t6y>
2025-08-02 06:55:02 +0100

    CARDS ok

--- src/app/sections/power-living-detail-section/power-living-detail-section.component.ts
+++ src/app/sections/power-living-detail-section/power-living-detail-section.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'app-power-living-detail-section',
     templateUrl: './power-living-detail-section.component.html',
     styleUrls: ['./power-living-detail-section.component.scss'],
 })
-export class PowerLivingDetailSectionComponent implements OnInit {
+export class PowerLivingDetailSectionComponent implements OnInit, OnChanges {
     @Input() powerLiving;
+    sortedArticles: any[] = [];
 
     ngOnInit(): void {}
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['powerLiving'] && this.powerLiving?.articles?.length) {
+            // Sort articles by date (newest first)
+            this.sortedArticles = this.powerLiving.articles.sort((a, b) => {
+                return new Date(b.date).getTime() - new Date(a.date).getTime();
+            });
+        }
+    }
 }
